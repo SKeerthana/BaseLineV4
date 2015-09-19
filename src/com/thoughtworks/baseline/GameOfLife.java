c3f17ee07499fc0a59c002eb5bad8f094600a231
keerthas <keerthas@INkeerthas.local>
2015-09-19 11:20:32 +0530

    Using checkForNeighboursCount to generate next state

--- src/com/thoughtworks/baseline/GameOfLife.java
+++ src/com/thoughtworks/baseline/GameOfLife.java
@@ -11,6 +11,7 @@ public class GameOfLife {
     public String[][] generateNextGeneration() {
         for (int i = 0; i < state.length; i++) {
             for (int j = 0; j < state[0].length; j++) {
+                int neighboursCount = checkForNeighboursCount(i,j);
             }
         }
         return state;
@@ -18,9 +19,9 @@ public class GameOfLife {
 
     public int checkForNeighboursCount(int i, int j) {
         int count = 0;
-        if (i - 1 >= 0 && state[i - 1][j - 1] == "X")
+        if (i - 1 >= 0 && j - 1 >= 0 && state[i - 1][j - 1] == "X")
             count++;
-        if (i - 1 >= 0 && state[i - 1][j] == "X")
+        if (i - 1 >= 0 && j - 1 >= 0 && state[i - 1][j] == "X")
             count++;
         if (j + 1 < state[0].length && i - 1 >= 0 && state[i - 1][j + 1] == "X")
             count++;
